83bf7286fec3d5582058a6441cbe6097c78421f1
Kacper Stępień <kacper2007x48@gmail.com>
2023-07-04 12:19:19 +0200

    add favicon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import Logo from "./utils/Logo";
 import Button from "./utils/Button";
 import Navigation from "./Navigation";
@@ -5,12 +7,32 @@ import Navigation from "./Navigation";
 import classes from "./Navbar.module.scss";
 
 const Navbar: React.FC = () => {
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+  const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+      if (window.innerWidth >= 750) setMobileMenuIsOpen(false);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [windowWidth]);
+
   return (
     <div className={classes.container}>
       <nav className={classes.navbar}>
         <Logo />
-        <Navigation />
-        <Button>Request Invite</Button>
+        <Navigation
+          windowWidth={windowWidth}
+          mobileMenuIsOpen={mobileMenuIsOpen}
+          setMobileMenuIsOpen={setMobileMenuIsOpen}
+        />
+        {windowWidth >= 750 && <Button>Request Invite</Button>}
       </nav>
     </div>
   );
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,56 @@
 import classes from "./Navigation.module.scss";
 
-const Navigation: React.FC = () => {
+type NavigationProps = {
+  windowWidth: number;
+  mobileMenuIsOpen: boolean;
+  setMobileMenuIsOpen: (value: boolean) => void;
+};
+
+const Navigation: React.FC<NavigationProps> = ({
+  windowWidth,
+  mobileMenuIsOpen,
+  setMobileMenuIsOpen,
+}) => {
+  const toggleMobileMenu = () => {
+    const open = mobileMenuIsOpen;
+    setMobileMenuIsOpen(!open);
+  };
+
+  if (windowWidth < 750)
+    return (
+      <>
+        <button className={classes.hamburgerMenu} onClick={toggleMobileMenu}>
+          {!mobileMenuIsOpen && (
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="11">
+              <g fill="#2D314D" fill-rule="evenodd">
+                <path d="M0 0h24v1H0zM0 5h24v1H0zM0 10h24v1H0z" />
+              </g>
+            </svg>
+          )}
+          {mobileMenuIsOpen && (
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="19">
+              <g fill="#2D314D" fill-rule="evenodd">
+                <path d="M.868.661l16.97 16.97-.706.708L.162 1.369z" />
+                <path d="M.161 17.632L17.131.662l.708.706-16.97 16.97z" />
+              </g>
+            </svg>
+          )}
+        </button>
+        {mobileMenuIsOpen && (
+          <>
+            <div className={classes.overlay}></div>
+            <ul className={classes.mobileMenu}>
+              <li className={classes.element}>Home</li>
+              <li className={classes.element}>About</li>
+              <li className={classes.element}>Contact</li>
+              <li className={classes.element}>Blog</li>
+              <li className={classes.element}>Careers</li>
+            </ul>
+          </>
+        )}
+      </>
+    );
+
   return (
     <ul className={classes.navigation}>
       <li className={classes.element}>Home</li>
